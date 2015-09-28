86ad71f34d82dce9032826584cf4f685ad2dda56
Ethanthecrazy <ethanthecrazy@gmail.com>
2015-09-27 21:52:32 -0400

    Im uploading because Spencer could not.

---
+++ Assets/Scripts/fpscontroller.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fpscontroller : MonoBehaviour {
+
+    [SerializeField]
+    float sensitivity = 1.0f;
+
+    [SerializeField]
+    float miny;
+
+    [SerializeField]
+    float maxy;
+
+    private float roty;
+    private float rotx;
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        rotx = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivity;
+
+        roty += Input.GetAxis("Mouse Y") * sensitivity;
+        roty = Mathf.Clamp(roty, miny, maxy);
+
+        transform.localEulerAngles = new Vector3(-roty, rotx, 0);
+	}
+}
