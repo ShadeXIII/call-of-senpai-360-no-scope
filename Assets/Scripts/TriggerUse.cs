1fe50ab6ca75c2f1cd4f7318100ac0ae4c0ef9ff
ShadeXIII <sm.shadexiii@gmail.com>
2015-10-15 18:37:12 -0600

    Added TriggerUse script going to work on it soon.

---
+++ Assets/Scripts/TriggerUse.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerUse : MonoBehaviour {
+
+    [SerializeField]
+    bool m_bOneUse;
+
+    [SerializeField]
+    float m_fUseTime;
+
+    [SerializeField]
+    GameObject[] m_tTarget;
+
+    [SerializeField]
+    bool m_bUseButton;
+
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+
+    void OnTriggerEnter(Collider collision)
+    {
+       // m_tTarget.GetComponent<Use>().Use();
+    }
+
+    void OnTriggerStay(Collider collision)
+    {
+        //m_tTarget.GetComponent<Use>().Use();
+    }
+
+}
